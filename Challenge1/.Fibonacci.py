e27a236960c63dadd0940b4b66b84fa038c3727e
mcoffey4999 <55113200+mcoffey4999@users.noreply.github.com>
2022-03-07 21:00:59 -0600

    Update .Fibonacci.py

--- Challenge1/.Fibonacci.py
+++ Challenge1/.Fibonacci.py
@@ -1,1 +1,13 @@
-
+int ans = 0;
+int start1 = 1;
+int start2 = 2;
+int initRes = 0;
+for(int i = 0; i <= 4000; i++){
+  if(start2 % 2 == 0){
+    ans += start2;
+  }
+  initRes = start1 + start2;
+  start1 = start2;
+  start2 = initRes;
+}
+Console.WriteLine(ans);
