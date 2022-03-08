985037dfc2c722606fa90e69d2e938ddb6e95639
mcoffey4999 <55113200+mcoffey4999@users.noreply.github.com>
2022-03-07 21:55:57 -0600

    Update Fibonacci.py

--- Challenge1/Fibonacci.py
+++ Challenge1/Fibonacci.py
@@ -2,12 +2,11 @@ ans = 2
 start1 = 1
 start2 = 2
 initRes = 0
-count = 0
-while count <= 4000000 or start2 <= 4000000:
+while start2 <= 4000000:
+    print(ans)
     initRes = start1 + start2
     start1 = start2
     start2 = initRes
-    count += 1
     if start2%2==0:
         ans += start2
 print(ans)
